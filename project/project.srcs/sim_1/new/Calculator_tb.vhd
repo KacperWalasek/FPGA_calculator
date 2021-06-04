a415427aedffbd3ced4fdeef175800cf0e8611bd
KacperWalasek <41973501+KacperWalasek@users.noreply.github.com>
2021-06-04 11:17:02 +0200

    Calculator na cyfrach

--- project/project.srcs/sim_1/new/Calculator_tb.vhd
+++ project/project.srcs/sim_1/new/Calculator_tb.vhd
@@ -1,43 +1,48 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.05.2021 17:00:52
--- Design Name: 
--- Module Name: Calculator_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Calculator_tb is
 --  Port ( );
 end Calculator_tb;
 
 architecture Behavioral of Calculator_tb is
-
+    signal clk      :std_logic  :='0';
+    signal R	    :std_logic  :='1';
+    signal input    :integer    := 0;
+    signal output   :integer    := 0;
+    signal number   :std_logic;
 begin
-
-
+    clk <= not clk after 500ns;
+    process is							
+    begin    
+    wait for 100 ns;
+    R <= '0';          
+    number <= '1';         
+    input <= 2; wait for 500 ns; 
+                                
+    number <= '0';       
+    input <= 1; wait for 500 ns; 
+                
+    number <= '1';                      
+    input <= 2; wait for 500 ns;    
+    
+    number <= '0';       
+    input <= 3; wait for 500 ns;  
+    
+    number <= '1';                      
+    input <= 2; wait for 500 ns; 
+    
+    number <= '0';       
+    input <= 1; wait for 500 ns;
+    R <='1';    wait for 400 ns;         
+            
+    end process;
+    Calc: entity work.Calculator
+        port map(
+            reset => R,
+            C=> clk,
+            input => input,
+            number => number,
+            output => output
+        );
 end Behavioral;
--- project/project.srcs/sources_1/new/Calculator.vhd
+++ project/project.srcs/sources_1/new/Calculator.vhd
@@ -1,34 +1,56 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 15.05.2021 16:59:38
--- Design Name: 
--- Module Name: Calculator - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Calculator is
---  Port ( );
+    Port ( 
+        reset : in std_logic;
+        C : in std_logic;
+        input : in natural;
+        number : in std_logic;
+        output : out integer     
+    );
 end Calculator;
-
+    
 architecture Behavioral of Calculator is
-
+    signal result : integer := 0;
+    signal current_expression : integer := 0;
+    signal last_sign : integer :=0;
+    
+    type STATE_T is (EXPRESSION_START, EXPRESSION_SIGN, EXPRESSION_NUMBER);
+    signal state : STATE_T := EXPRESSION_START;
 begin
-
-
+    process(reset, C)
+    begin
+        if reset = '1' then
+            state <= EXPRESSION_START;
+            result <= 0;
+        elsif C'event then
+            case state is
+                when EXPRESSION_START =>
+                    current_expression <= input;
+                    state <= EXPRESSION_NUMBER;
+                when EXPRESSION_NUMBER =>
+                    last_sign <= input;
+                    case input is
+                        when 1 => -- +
+                            result <= result + current_expression;
+                            current_expression <= 0;
+                            state <= EXPRESSION_START;
+                        when 2 =>  -- -
+                            result <= result - current_expression;
+                            current_expression <= 0;
+                            state <= EXPRESSION_START;
+                        when 3 => -- *
+                            state <= EXPRESSION_SIGN;
+                        when others=>
+                            
+                    end case;                   
+                when EXPRESSION_SIGN =>
+                    current_expression <= current_expression * input; 
+                    state <= EXPRESSION_NUMBER;
+            end case;
+        end if;
+    end process;
+    
+    output <= result + current_expression;
 end Behavioral;
